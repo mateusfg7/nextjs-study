15c0c1dde7fd7b8933d9f5daf9c70648a792fc22
mateusfg7 <mateusfelipefg77@gmail.com>
2020-10-19 16:25:12 -0300

    init project

---
+++ reactsetup/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Homepage</title>
+      </Head>
+
+      <main>
+        <h1>Hello Word</h1>
+      </main>
+    </div>
+  );
+}
