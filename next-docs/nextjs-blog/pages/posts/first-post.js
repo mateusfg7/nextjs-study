35c62f3639abfbc3ba9c6dd9e61cb95341c7e955
mateusfg7 <mateusfelipefg77@gmail.com>
2020-10-20 10:15:35 -0300

    Layout Component

--- next-docs/nextjs-blog/pages/posts/first-post.js
+++ next-docs/nextjs-blog/pages/posts/first-post.js
@@ -1,9 +1,11 @@
 import Head from 'next/head';
 import Link from 'next/link';
 
+import Layout from '../../components/layout';
+
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
       <Head>
         <title>First Post</title>
       </Head>
@@ -13,6 +15,6 @@ export default function FirstPost() {
           <a>Back to home</a>
         </Link>
       </h2>
-    </>
+    </Layout>
   );
 }
