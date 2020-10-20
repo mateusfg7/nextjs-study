1dab570d4eeb753884b28401880ecf3dd73a15d1
mateusfg7 <mateusfelipefg77@gmail.com>
2020-10-20 10:01:59 -0300

    Metadadta

--- next-docs/nextjs-blog/pages/posts/first-post.js
+++ next-docs/nextjs-blog/pages/posts/first-post.js
@@ -1,8 +1,12 @@
+import Head from 'next/head';
 import Link from 'next/link';
 
 export default function FirstPost() {
   return (
     <>
+      <Head>
+        <title>First Post</title>
+      </Head>
       <h1>First Post</h1>
       <h2>
         <Link href='/'>
