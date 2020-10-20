ef5b17993e01c99d10f9c23b796027b7b99b98cd
mateusfg7 <mateusfelipefg77@gmail.com>
2020-10-20 19:39:09 -0300

    Dynamic Routes

--- next-docs/nextjs-blog/pages/index.js
+++ next-docs/nextjs-blog/pages/index.js
@@ -1,6 +1,11 @@
 import Head from 'next/head';
+import Link from 'next/link';
+
 import Layout, { siteTitle } from '../components/layout';
+import Date from '../components/date';
+
 import utilStyles from '../styles/utils.module.css';
+
 import { getSortedPostsData } from '../lib/posts';
 
 export default function Home({ allPostsData }) {
@@ -24,11 +29,13 @@ export default function Home({ allPostsData }) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
